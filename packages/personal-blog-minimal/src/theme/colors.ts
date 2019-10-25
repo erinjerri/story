a6cf971e47fdc5bf2882b99e34480df6f1a13ab2
Erin <erin@erinjerri.com>
2019-10-25 14:19:49 -0700

    change icon color
    color hex replace

--- packages/personal-blog-minimal/src/theme/colors.ts
+++ packages/personal-blog-minimal/src/theme/colors.ts
@@ -9,8 +9,8 @@ const colors = {
   inactiveBG: "#e6e6e6", // 7
   borderColor: "#DBDBDB", // 8
   lightBorderColor: "#ededed", //9
-  primary: "#D10068", // 10
-  primaryHover: "#D10068", // 11
+  primary: "##00B6A9", // 10
+  primaryHover: "##00B6A9", // 11
 }
 
 export default colors
